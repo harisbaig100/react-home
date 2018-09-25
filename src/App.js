4bdb0b0b490cf82aedfa2895eeae716a9a803447
Lance Cho <cholance1022@gmail.com>
2018-09-24 23:17:27 -0700

    initial commit to add navbar material UI.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
@@ -10,6 +13,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                React & Material-UI Sample Application
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
