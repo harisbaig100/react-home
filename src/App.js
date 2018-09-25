8cd50a53d51e591968907a3beffdaf07e01cbec8
Lance Cho <cholance1022@gmail.com>
2018-09-24 23:29:36 -0700

    refactored navbar, moved to its own component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
@@ -14,13 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="title" color="inherit">
-                React & Material-UI Sample Application
-              </Typography>
-            </Toolbar>
-          </AppBar>
+          <NavBar></NavBar>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
