17f4a0e99b5433e81210ab08385878586bed013b
Lance Cho <cholance1022@gmail.com>
2018-09-25 21:42:17 -0700

    initial facebook login implementation. deleted unnecessary code. added react-facebook-login npm package.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,19 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Login from './Login';
 
 const NavBar = () => {
     return(
         <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                React & Material-UI Sample Application
-                </Typography>
-            </Toolbar>
-        </AppBar>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="title" color="inherit">
+                        React & Material-UI Sample Application
+                    </Typography>
+                        <Login></Login>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
